ccadaee636dff9afde43f8a5f151194cc118df20
Alexei Karyavin <alexis-karyavin@yandex.ru>
2019-12-25 20:59:36 +0300

    Last Commit

--- src/assets/Classes/CreatorCar.ts
+++ src/assets/Classes/CreatorCar.ts
@@ -41,28 +41,4 @@ export class CreatorMercedes extends Creator{
   }
 }
 
-// export class CreatorMercedes extends CreatorCar{
-//   private car: any;
-//   constructor(model: string) {
-//     super(model);
-//     if (model === "Benz S Class") {
-//       this.car = new CarMercedes("Benz S Class", 3180000);
-//     }
-//   }
-//   public create(): Car {
-//     return this.car;
-//   }
-// }
-
-// export class CreatorBMW_M3 implements CreatorCar {
-//   public init(): Car {
-//     return new CarBMW("M3", 4230000);
-//   }
-// }
-//
-// export class CreatorMercedes_Benz_S implements CreatorCar {
-//   public init(): Car {
-//     return new CarMercedes("Benz S Class", 3180000);
-//   }
-// }
 
